7b7c11389a4157b326f4240321aae69649ed1b17
Francesca Polzella <franpolzella@gmail.com>
2024-12-10 01:56:12 -0400

    modificado con dos archivo nuevos

--- js/alerta.js
+++ js/alerta.js
@@ -3,7 +3,7 @@
 
     if(!alerta){
       const alert = document.createElement('p')
-        alert.classList.add('bg-red-100')
+        alert.classList.add('bg-red-100','border-red-400', 'text-red-700', 'px-4','py-3','rounded','text-center')
         alert.innerHTML= mensaje
         formulario.appendChild(alert)
 
